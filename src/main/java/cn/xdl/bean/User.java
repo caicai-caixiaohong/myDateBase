695349f61261365df3d24fff0f737a2a7c2d43da
caicai-caixiaohong <1334261455@qq.com>
2019-11-14 00:46:10 +0800

    实现了序列化接口

--- src/main/java/cn/xdl/bean/User.java
+++ src/main/java/cn/xdl/bean/User.java
@@ -1,6 +1,12 @@
 package cn.xdl.bean;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private String name;
 	private String password;
