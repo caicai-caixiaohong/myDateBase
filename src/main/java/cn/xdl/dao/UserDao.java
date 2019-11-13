2861cbafb53693e94e7a59ff55e0e5e03cdc6a4b
caicai-caixiaohong <1334261455@qq.com>
2019-11-14 02:14:13 +0800

    修改了dao

--- src/main/java/cn/xdl/dao/UserDao.java
+++ src/main/java/cn/xdl/dao/UserDao.java
@@ -5,5 +5,6 @@ import org.apache.ibatis.annotations.Param;
 import cn.xdl.bean.User;
 
 public interface UserDao {
-	User findUserByNameAndPassword(@Param("name")String name,@Param("password")String password);
+	User findUserByNameAndPassword(
+			@Param("name")String name,@Param("password")String password);
 }
